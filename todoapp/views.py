b44e238afb427787bee8decc9569591d33f4d7c5
nora-tiger <125980258+nora-tiger@users.noreply.github.com>
2023-05-10 16:30:56 +0530

    Initial commit

---
+++ todoapp/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+
+from .forms import TodoForm
+from .models import Task
+
+# Create your views here.
+def home(request):
+    task1 = Task.objects.all()
+    if request.method=="POST":
+        name=request.POST.get("task")
+        priority=request.POST.get("priority")
+        date=request.POST.get("date")
+        task=Task(name=name,priority=priority,date=date)
+        task.save()
+    return render(request,'home.html',{'task1':task1})
+
+
+def delete(request,task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == "POST":
+        task.delete()
+        return redirect('/')
+    return render(request, "delete.html")
+
+def update(request,id):
+    task = Task.objects.get(id=id)
+
+    form=TodoForm(request.POST or None,instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request, "update.html",{'form':form,'task':task})
+
